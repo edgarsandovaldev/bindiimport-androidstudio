f979af893b9fcffbe1336d4c639009e2b0059477
Edgar Sandoval Desarrollador <59340481+edgarsandovaldev@users.noreply.github.com>
2022-10-28 14:54:12 +0000

    Ajuste de Contacto al lado derecho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,7 @@ import { ProductosComponent } from './secciones/productos/productos.component';
 import { NavLeftComponent } from './components/nav-left/nav-left.component';
 import { NavLeftProductsComponent } from './components/nav-left-products/nav-left-products.component';
 import { NavLeftBindiComponent } from './components/nav-left-bindi/nav-left-bindi.component';
-import { TodosComponent } from './modules/prroductos/todos/todos.component';
-import { MarcasComponent } from './modules/prroductos/marcas/marcas.component';
+
 
 
 @NgModule({
@@ -39,8 +38,7 @@ import { MarcasComponent } from './modules/prroductos/marcas/marcas.component';
     NavLeftComponent,
     NavLeftProductsComponent,
     NavLeftBindiComponent,
-    TodosComponent,
-    MarcasComponent
+
   ],
   imports: [
     BrowserModule,
