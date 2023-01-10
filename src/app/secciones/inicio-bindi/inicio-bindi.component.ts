0da832a8ca57142514034281ef567e78d3e3dccb
Edgar Sandoval Developer <59340481+edgarsandovaldev@users.noreply.github.com>
2023-01-10 16:11:57 -0500

    Cambios con footer

---
+++ src/app/secciones/inicio-bindi/inicio-bindi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-bindi',
+  templateUrl: './inicio-bindi.component.html',
+  styleUrls: ['./inicio-bindi.component.css']
+})
+export class InicioBindiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
