6886cc2e492c8587a2b7faac08fb731bcb902ff7
Edgar Sandoval Developer <59340481+edgarsandovaldev@users.noreply.github.com>
2023-01-16 09:03:47 -0500

    actualizaciones de efectos

--- src/app/secciones/inicio/inicio.component.ts
+++ src/app/secciones/inicio/inicio.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
+
 export class InicioComponent implements OnInit {
 
+  titulo: String;
+  subTitulo: string;
+  
+
   constructor() {
+    this.titulo = 'Nuestros Aliados';
+    this.subTitulo = 'subtitulo de la app';     
 
    }
 
   ngOnInit(): void {
   }
-
-
 
 
 }
