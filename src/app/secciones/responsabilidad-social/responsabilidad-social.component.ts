0eefff5a71a742049b859be3374a9b24e884cf43
Edgar Sandoval Developer <59340481+edgarsandovaldev@users.noreply.github.com>
2023-01-20 18:01:14 +0100

    Seccion completa de Socioambiental

--- src/app/secciones/responsabilidad-social/responsabilidad-social.component.ts
+++ src/app/secciones/responsabilidad-social/responsabilidad-social.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-responsabilidad-social',
   templateUrl: './responsabilidad-social.component.html',
-  styleUrls: ['./responsabilidad-social.component.css']
+  styleUrls: ['./responsabilidad-social.component.css'],
+  animations:[]
 })
 export class ResponsabilidadSocialComponent implements OnInit {
+
+  public estado:string = "inactivo";
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toglebutton() {
+
+  };
 
 }
