547f5c6e1dddf4e28834308f1d1682abdb163412
Edgar Sandoval Developer <59340481+edgarsandovaldev@users.noreply.github.com>
2023-01-21 05:48:19 +0100

    viernes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -44,6 +45,7 @@ import { InicioBindiComponent } from './secciones/inicio-bindi/inicio-bindi.comp
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
